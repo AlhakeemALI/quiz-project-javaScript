9af63079133d48bbe11f6d4a9a129099fc021a0d
Ali <90877977+AlhakeemALI@users.noreply.github.com>
2023-01-23 13:55:55 -0700

    add all the data need

---
+++ script.js
@@ -0,0 +1,106 @@
+// Creat array for all the questions
+
+
+const data = [
+  {
+    id:1 ,
+    question: "What is the name for the Jewish New Year?",
+    answers: [
+      {answer: "Hanukkah", isCorrect:false},
+      {answer: "Yom Kippur", isCorrect:false},
+      {answer: "Kwanza",isCorrect:false},
+      {answer: "Rosh Hashanah", isCorrect:true}
+    ]
+  },
+  {
+    id:2 ,
+    question: "How many blue stripes are there on the U.S. flag?",
+    answers: [
+      {answer: "6", isCorrect:false},
+      {answer: "7",isCorrect:false},
+      {answer: "13",isCorrect:true},
+      {answer: "0",isCorrect:false}
+    ]
+  },
+  {
+    id:3 ,
+    question: "Which one of these characters is not friends with Harry Potter?",
+    answers: [
+      {answer: "Draco Malfoy", isCorrect:true},
+      {answer: "Ron Weasley",isCorrect:false},
+      {answer: "Neville Longbottom",isCorrect:false},
+      {answer: "Hermione Granger",isCorrect:false}
+    ]
+  },
+  {
+    id:4 ,
+    question: "What was the name of the band Lionel Richie was a part of?",
+    answers: [
+      {answer: "King Harvest", isCorrect:false},
+      {answer: "Spectrums", isCorrect:false},
+      {answer: "Commodores", isCorrect:true},
+      {answer: "The Marshall Tucker Band",isCorrect:false}
+    ]
+  },
+  {
+    id:5 ,
+    question: "Which animal does not appear in the Chinese zodiac?",
+    answers: [
+      {answer: "Brazil",isCorrect:true},
+      {answer: " Italy",isCorrect:false},
+      {answer: "Ireland",isCorrect:false},
+      {answer: "China",isCorrect:false}
+    ]
+  },
+  {
+    id:6 ,
+    question: "Which planet is the hottest?",
+    answers: [
+      {answer: "Saturn",isCorrect:false},
+      {answer: "Venus",isCorrect:true},
+      {answer: "Mercury",isCorrect:false},
+      {answer: "Mars",isCorrect:false}
+    ]
+  },
+  {
+    id:7 ,
+    question: "Who was the only U.S. President to resign?",
+    answers: [
+      {answer: "Herbert Hoover",isCorrect:false},
+      {answer: "Richard Nixon",isCorrect:true},
+      {answer: "George W. Bush",isCorrect:false},
+      {answer: "Barack Obama",isCorrect:false}
+    ]
+  },
+  {
+    id:8 ,
+    question: "What does the “D” in “D-Day” stand for?",
+    answers: [
+      {answer: "Dooms",isCorrect:false},
+      {answer: "Dark",isCorrect:false},
+      {answer: "Day ",isCorrect:true},
+      {answer: "Dunkirk",isCorrect:false}
+    ]
+  },
+  {
+    id:9 ,
+    question: "Which water sport is the official sport for the state of Hawaii?",
+    answers: [
+      {answer: "water polo",isCorrect:false},
+      {answer: "swimming",isCorrect:false},
+      {answer: "water skiing",isCorrect:false},
+      {answer: "surfing",isCorrect:true}
+    ]
+  },
+  {
+    id:10 ,
+    question: "What is the name of the company that published the Mario Kart video game?",
+    answers: [
+      {answer: "Nintendo",isCorrect:false},
+      {answer: "Electronic Arts (EA)",isCorrect:false},
+      {answer: "SEGA",isCorrect:true},
+      {answer: " Xbox",isCorrect:false}
+    ]
+  },
+]
+
